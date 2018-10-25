05e493c542c73dcfeb732b29574e11f7cb955c58
ebotcode <ebotcode@gmail.com>
2018-10-25 01:26:37 -0700

    working codes

--- sdc.py
+++ sdc.py
@@ -56,4 +56,5 @@ color_edges = np.dstack((edges, edges, edges))
 # Draw the lines on the edge image
 lines_edges = cv2.addWeighted(color_edges, 0.8, line_image, 1, 0) 
 plt.imshow(lines_edges)
+plt.show()
 
